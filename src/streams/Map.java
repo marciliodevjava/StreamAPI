5b84b63a646d17e12a138c38917d1dc58fd18f4e
m-arc <m-arc@192.168.1.8>
2022-04-30 10:19:40 -0300

    Atualização

--- src/streams/Map.java
+++ src/streams/Map.java
@@ -3,7 +3,6 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.UnaryOperator;
 
 public class Map {
 
@@ -14,9 +13,14 @@ public class Map {
 		
 		marcas.stream().map(m -> m.toUpperCase()).forEach(print);
 		
-		UnaryOperator<String> maiscula = n -> n.toUpperCase();
-		UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
-		UnaryOperator<String> grito = n -> n + "!!!!";
+//		UnaryOperator<String> maiscula = n -> n.toUpperCase();
+//		UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+//		UnaryOperator<String> grito = n -> n + "!!!! ";
 		
+		System.out.println();
+		System.out.println(Utilitarios.maiscula.andThen(Utilitarios.primeiraLetra).andThen(Utilitarios::grito).apply("BMW"));
+		
+		System.out.println("\n\nUsando composição.....");
+		marcas.stream().map(Utilitarios.maiscula).map(Utilitarios.primeiraLetra).map(Utilitarios::grito).forEach(print);
 	}
 }
