f2016d3ab8237782bb73cda471e816029cc72367
m-arc <m-arc@192.168.1.8>
2022-04-30 15:15:37 -0300

    Desafio Aula 201

---
+++ src/streams/DesafioAula201ClassAluno.java
@@ -0,0 +1,16 @@
+package streams;
+
+public class DesafioAula201ClassAluno {
+
+	final String nome;
+	final double nota1;
+	final double nota2;
+
+	public DesafioAula201ClassAluno(String nome, double nota1, double nota2) {
+		super();
+		this.nome = nome;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+
+}
---
+++ src/streams/DesafioFilterAula201.java
@@ -0,0 +1,38 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class DesafioFilterAula201 {
+
+	public static void main(String[] args) {
+
+		DesafioAula201ClassAluno a1 = new DesafioAula201ClassAluno("Luclia", 9.5, 9.8);
+		DesafioAula201ClassAluno a2 = new DesafioAula201ClassAluno("Raimundo", 7.5, 8.8);
+		DesafioAula201ClassAluno a3 = new DesafioAula201ClassAluno("Matheus", 5.5, 8.8);
+		DesafioAula201ClassAluno a4 = new DesafioAula201ClassAluno("Marcelo", 7.5, 7.8);
+		DesafioAula201ClassAluno a5 = new DesafioAula201ClassAluno("Marcilio", 9.5, 7.8);
+		DesafioAula201ClassAluno a6 = new DesafioAula201ClassAluno("Macauly", 4.5, 7.8);
+		DesafioAula201ClassAluno a7 = new DesafioAula201ClassAluno("Gleice", 5.5, 6.8);
+		DesafioAula201ClassAluno a8 = new DesafioAula201ClassAluno("Alessandra", 4.5, 7.8);
+		DesafioAula201ClassAluno a9 = new DesafioAula201ClassAluno("Isabela", 9.5, 7.8);
+		DesafioAula201ClassAluno a10 = new DesafioAula201ClassAluno("Laura", 6.5, 7.8);
+
+		List<DesafioAula201ClassAluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+
+		Predicate<DesafioAula201ClassAluno> aprovado = a -> (a.nota1 + a.nota2) / 2 >= 7
+				&& (a.nota1 + a.nota2) / 2 < 10;
+		Predicate<DesafioAula201ClassAluno> reprovado = a -> (a.nota1 + a.nota2) / 2 >= 0
+				&& (a.nota1 + a.nota2) / 2 < 7;
+		Function<DesafioAula201ClassAluno, String> saudacaoAprovados = a -> "Parabens " + a.nome
+				+ " Você foi aprovado!";
+		Function<DesafioAula201ClassAluno, String> saudacaoReprovado = a -> "Infelizmente " + a.nome
+				+ ", você está Reprovado!";
+
+		alunos.stream().filter(aprovado).map(saudacaoAprovados).forEach(System.out::println);
+		System.out.println();
+		alunos.stream().filter(reprovado).map(saudacaoReprovado).forEach(System.out::println);
+	}
+}
