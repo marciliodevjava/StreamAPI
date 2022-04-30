c82c53c43b3f8d189d4fd77ae7c9dd4d40e24db5
m-arc <m-arc@192.168.1.8>
2022-04-30 09:27:54 -0300

    Atualização

--- src/streams/CriandoStreams.java
+++ src/streams/CriandoStreams.java
@@ -1,6 +1,7 @@
 package streams;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -9,6 +10,7 @@ public class CriandoStreams {
 	public static void main(String[] args) {
 
 		Consumer<String> print = System.out::print;
+		Consumer<Integer> println = System.out::print;
 
 		// Stream.of passando os valores literais
 		Stream<String> langs = Stream.of("Java ", "Lua ", "JS\n");
@@ -16,10 +18,18 @@ public class CriandoStreams {
 		langs.forEach(print);
 
 		String[] maisLangs = { "Pyton ", "Lisp ", "Pelr ", "Go\n" };
-		
+
 		// Stream.of passando um array
 		Stream.of(maisLangs).forEach(print);
 		// Arrays.Stream passando as informações necessárias
 		Arrays.stream(maisLangs).forEach(print);
+		Arrays.stream(maisLangs, 1, 4).forEach(print);
+
+		List<String> outrasLagns = Arrays.asList("C ", "PHP ", "Kotlin\n");
+		outrasLagns.stream().forEach(print);
+		outrasLagns.parallelStream().forEach(print);// Stream paralela
+
+		// Stream.generate(() -> "a").forEach(print); // Stream de forma infinita
+		Stream.iterate(0, n -> n + 1).forEach(println);
 	}
 }
