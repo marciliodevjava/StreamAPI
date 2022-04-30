1f3e258c85155955496bcfa0230b1669230fc479
m-arc <m-arc@192.168.1.8>
2022-04-30 15:23:52 -0300

    Atualização

--- src/streams/DesafioFilterAula201.java
+++ src/streams/DesafioFilterAula201.java
@@ -16,22 +16,30 @@ public class DesafioFilterAula201 {
 		DesafioAula201ClassAluno a5 = new DesafioAula201ClassAluno("Marcilio", 9.5, 7.8);
 		DesafioAula201ClassAluno a6 = new DesafioAula201ClassAluno("Macauly", 4.5, 7.8);
 		DesafioAula201ClassAluno a7 = new DesafioAula201ClassAluno("Gleice", 5.5, 6.8);
-		DesafioAula201ClassAluno a8 = new DesafioAula201ClassAluno("Alessandra", 4.5, 7.8);
+		DesafioAula201ClassAluno a8 = new DesafioAula201ClassAluno("Alessandra", 4.5, 3.4);
 		DesafioAula201ClassAluno a9 = new DesafioAula201ClassAluno("Isabela", 9.5, 7.8);
 		DesafioAula201ClassAluno a10 = new DesafioAula201ClassAluno("Laura", 6.5, 7.8);
 
 		List<DesafioAula201ClassAluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
 
-		Predicate<DesafioAula201ClassAluno> aprovado = a -> (a.nota1 + a.nota2) / 2 >= 7
+		Predicate<DesafioAula201ClassAluno> nota1 = a -> a.nota1 >= 7
 				&& (a.nota1 + a.nota2) / 2 < 10;
-		Predicate<DesafioAula201ClassAluno> reprovado = a -> (a.nota1 + a.nota2) / 2 >= 0
+		Predicate<DesafioAula201ClassAluno> nota2 = a -> a.nota1>= 7
+				&& (a.nota1 + a.nota2) / 2 < 10;
+		Predicate<DesafioAula201ClassAluno> recuperação = a -> (a.nota1 + a.nota2) / 2 >= 4
 				&& (a.nota1 + a.nota2) / 2 < 7;
+		Predicate<DesafioAula201ClassAluno> reprovado = a -> (a.nota1 + a.nota2) / 2 >= 0
+				&& (a.nota1 + a.nota2) / 2 < 4;
 		Function<DesafioAula201ClassAluno, String> saudacaoAprovados = a -> "Parabens " + a.nome
 				+ " Você foi aprovado!";
+		Function<DesafioAula201ClassAluno, String> saudacaoRecuperacao = a -> "Infelizmente " + a.nome
+				+ ", você está de Recuperação!";
 		Function<DesafioAula201ClassAluno, String> saudacaoReprovado = a -> "Infelizmente " + a.nome
 				+ ", você está Reprovado!";
 
-		alunos.stream().filter(aprovado).map(saudacaoAprovados).forEach(System.out::println);
+		alunos.stream().filter(nota1).filter(nota2).map(saudacaoAprovados).forEach(System.out::println);
+		System.out.println();
+		alunos.stream().filter(recuperação).map(saudacaoRecuperacao).forEach(System.out::println);
 		System.out.println();
 		alunos.stream().filter(reprovado).map(saudacaoReprovado).forEach(System.out::println);
 	}
