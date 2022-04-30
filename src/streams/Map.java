2d32b83454a6b605fe7b3ad66b8f436243e52c41
m-arc <m-arc@192.168.1.8>
2022-04-30 10:02:55 -0300

    Atualização

--- src/streams/Map.java
+++ src/streams/Map.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class Map {
 
@@ -12,5 +13,10 @@ public class Map {
 		List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda ");
 		
 		marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+		
+		UnaryOperator<String> maiscula = n -> n.toUpperCase();
+		UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+		UnaryOperator<String> grito = n -> n + "!!!!";
+		
 	}
 }
