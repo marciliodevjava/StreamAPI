87b086dc012b5c73b4efddd53228ce59ec23983b
m-arc <m-arc@192.168.1.8>
2022-04-30 22:40:01 -0300

    Stream API Reduce

---
+++ src/streams/Media.java
@@ -0,0 +1,24 @@
+package streams;
+
+public class Media {
+
+	private double total;
+	private int quantidade;
+
+	public Media adicionar(double valor) {
+		this.total += valor;
+		this.quantidade++;
+		return this;
+	}
+
+	public double getValor() {
+		return this.total / this.quantidade;
+	}
+
+	public static Media combinar(Media m1, Media m2) {
+		Media resultante = new Media();
+		resultante.total = m1.total + m2.total;
+		resultante.quantidade = m1.quantidade + m2.quantidade;
+		return resultante;
+	}
+}
